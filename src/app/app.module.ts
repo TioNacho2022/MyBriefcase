313258bbb955e97a4994b625e9f3c36f14f26ea0
TioNacho2022 <cris.leiva@duocuc.cl>
2023-09-12 20:31:06 -0300

    Version 1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { MainComponent } from './component/main/main.component';
 import { InformationComponent } from './component/information/information.component';
 import { FinmeComponent } from './component/finme/finme.component';
-
 
 
 
@@ -23,7 +21,12 @@ import { FinmeComponent } from './component/finme/finme.component';
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
