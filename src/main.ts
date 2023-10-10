57c1ae4173b5db53d2fbecdd1a3c748fee49ac48
TioNacho2022 <eguiluz191@gmail.com>
2023-10-10 01:46:30 -0300

    Cambios de version

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,25 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { MainComponent } from './app/component/main/main.component';
+import { InformationComponent } from './app/component/information/information.component';
+import { FinmeComponent } from './app/component/finme/finme.component';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then((moduleRef) => {
+  // Registra los componentes personalizados como elementos personalizados
+  const mainComponent = createCustomElement(MainComponent, { injector: moduleRef.injector });
+  customElements.define('app-main', mainComponent);
+
+  const informationComponent = createCustomElement(InformationComponent, { injector: moduleRef.injector });
+  customElements.define('app-information', informationComponent);
+
+  const finmeComponent = createCustomElement(FinmeComponent, { injector: moduleRef.injector });
+  customElements.define('app-finme', finmeComponent);
+
+  // Define otros elementos personalizados aquí si es necesario
+})
+.catch(err => console.error(err));
